bf60312aa4f202b3015214ade56a65b7e5c9ffa7
reginakasteen <albina.kostyuchenko03@gmail.com>
2026-01-10 02:31:30 +0200

    homework #3. task 2 with no styles

--- hw_3/script.js
+++ hw_3/script.js
@@ -61,12 +61,104 @@ class TransportFactory {
 }
 
 
-const transportCar = TransportFactory.create('car');
-transportCar.ride();
-transportCar.stop();
-console.log('\n');
-const transportTrain = TransportFactory.create('train');
-transportTrain.ride();
-transportTrain.stop();
-const transportUnknown = TransportFactory.create('kick scooter');
+// const transportCar = TransportFactory.create('car');
+// transportCar.ride();
+// transportCar.stop();
+// console.log('\n');
+// const transportTrain = TransportFactory.create('train');
+// transportTrain.ride();
+// transportTrain.stop();
+//const transportUnknown = TransportFactory.create('kick scooter');
 
+
+//__________________________________________________
+console.log('\n\n2.');
+
+const app = document.getElementById('app');
+
+const button = document.getElementById('display-btn');
+const previous = document.getElementById('prev');
+const page = document.getElementById('page');
+const next = document.getElementById('next');
+
+previous.hidden = true;
+page.hidden = true;
+next.hidden = true;
+
+let currentPage = 1;
+let nextPageUrl = null;
+let prevPageUrl = null;
+
+function renderPage(url) {
+
+    const response = fetch(url);
+    const parsedData = response.then(data => {
+        if (data.status !== 200){
+            alert('Error')
+        }
+        console.log(data);
+        return data.json();
+    })
+    .catch(error => {
+        console.log('Error:', error);
+    })
+    .finally(() => {
+        console.log('Request processed');
+
+    });
+
+    parsedData.then(data => {
+    
+    app.innerText = '';
+    data.results.forEach(item => {
+        app.innerHTML += `<div><p>${item.name}</p><p>${item.status}</p><img src="${item.image}"></div>`;
+    })
+ 
+    nextPageUrl = data.info.next;
+    prevPageUrl = data.info.prev;
+
+    if (data.info.next) {
+        page.innerText = currentPage;
+    } else {
+        page.innerText = data.info.pages;
+    }
+
+    previous.disabled = !prevPageUrl;
+    next.disabled = !nextPageUrl;
+
+    });
+}
+
+button.addEventListener('click', () => {
+
+    button.remove();
+    app.innerText = "Loading...";
+
+    previous.hidden = false;
+    page.hidden = false;
+    next.hidden = false;
+
+    next.addEventListener('click', () => {
+        if (nextPageUrl) {
+            currentPage++;
+            renderPage(nextPageUrl);
+        }
+    });
+
+    previous.addEventListener('click', () => {
+        if (prevPageUrl) {
+            currentPage--;
+            renderPage(prevPageUrl);
+        }
+    });
+
+renderPage(`https://rickandmortyapi.com/api/character?page=${currentPage}`);
+
+
+});
+
+
+
+
+
+        
