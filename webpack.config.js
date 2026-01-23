f27c4c26171dda87560b74b840e97f43edbe2aec
reginakasteen <albina.kostyuchenko03@gmail.com>
2026-01-23 16:19:15 +0200

    config fixed

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env, argv) => {
   return {
     entry: "./src/index.js",
     output: {
-      path: path.resolve(__dirname, "dist"),
+      path: path.resolve(__dirname, "docs"),
       filename: "bundle.[contenthash].js",
       publicPath: "/",
       clean: true,
